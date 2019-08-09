4583e524980715223f05a77b84a063937e27520b
Jeff Githiri <geoffrey.githiri@andela.com>
2019-08-09 10:15:28 +0300

    configure port variable for heroku

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
-
 const path = require('path');
 
 const publicPath = path.join(__dirname, '../public');
+
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(publicPath));
+
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+    console.log(`server started on port: ${port} `)
+});
 
